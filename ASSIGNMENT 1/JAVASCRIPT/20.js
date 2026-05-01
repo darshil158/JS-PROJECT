b59321998be6ab7a0a4c125015152de876dc8ffd
Darshil Rana <darshilrana158@gmail.com>
2026-05-01 18:20:47 +0530

    Update 20.js

--- ASSIGNMENT 1/JAVASCRIPT/20.js
+++ ASSIGNMENT 1/JAVASCRIPT/20.js
@@ -1,14 +1,13 @@
 /* Write a program to convert minutes into hours and minutes.*/
 
-
 let min = 135;
-let hr;
-let rMin;
+let hr = 0;
 
-hr = min / 60;
-rMin = min % 60;
-
+while (min >= 60) {
+    min = min - 60;
+    hr = hr + 1;
+}
 
 console.log("Minutes :- " + min + " min");
 console.log("Hours :- " + hr + " hr");
-console.log("Remaining Minutes :- " + rMin + " min");
+console.log("Remaining Minutes :- " + min + " min");
