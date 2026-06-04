d4673df9923ee22decfa053a063db65ebd5591fd
Darshil Rana <darshilrana158@gmail.com>
2026-06-04 16:52:43 +0530

    Update script.js

--- Countdown & Quotes Application/Timer/js/script.js
+++ Countdown & Quotes Application/Timer/js/script.js
@@ -1,11 +1,11 @@
-var targetDate = new Date(2026, 11, 31, 0, 0, 0);
+let targetDate = new Date(2027, 1, 1, 0, 0, 0);
 
-var interval = setInterval(() => {
+let interval = setInterval(() => {
 
-    var now = new Date();
+    let now = new Date();
 
     now = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds());
-    var diffMs = targetDate - now;
+    let diffMs = targetDate - now;
 
     if (diffMs <= 0) {
         clearInterval(interval);
@@ -14,13 +14,13 @@ var interval = setInterval(() => {
         return;
     }
 
-    var diffSec = parseInt(diffMs / 1000);
-    var diffMin = parseInt(diffSec / 60);
-    var diffHour = parseInt(diffMin / 60);
+    let diffSec = parseInt(diffMs / 1000);
+    let diffMin = parseInt(diffSec / 60);
+    let diffHour = parseInt(diffMin / 60);
 
-    var seconds = diffSec % 60;
-    var minutes = diffMin % 60;
-    var hours = diffHour;
+    let seconds = diffSec % 60;
+    let minutes = diffMin % 60;
+    let hours = diffHour;
 
     if (hours < 10) {
         hours = "0" + hours;
